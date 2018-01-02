21b5197e57389e1aac99c89a66be61e04d6d3634
j265 <j2566sm@gmail.com>
2018-01-02 23:11:49 +0000

    Edit Tue 2 Jan 2018 23:11:49 GMT

--- FB-U.sh
+++ FB-U.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 curl -s https://fanboy.co.nz/r/fanboy-ultimate.txt | awk '!/xmlhttprequest,domain=pornhub.com/' | sed '1,12d' | sed '1i\
-! Title: FB Ultimate list.txt' > /Users/rayyan/Gitlab/mf/FB-ultimate.txt
+! Title: FB Ultimate list.txt' > /Users/rayyan/Gitlab/mf/FB-U.txt
